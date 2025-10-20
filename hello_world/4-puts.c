37ebab7d0c389c98b4312cc3866b85e5af38cd48
ValentinCA28 <12286@holbertonstudents.com>
2025-10-20 14:13:21 +0200

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
 * main - Entry point
+*
 * Description: 'print a string using puts function'
 * Return: Always 0 (Sucess)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
