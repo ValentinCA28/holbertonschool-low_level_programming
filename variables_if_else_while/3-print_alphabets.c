2536afbb2c9294354b8f2e259d82c1de3d54c34f
ValentinCA28 <12286@holbertonstudents.com>
2025-10-21 14:51:27 +0200

    task 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print alphabet minuscule et majuscules
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c, i;
+{
+for (i = 'a' ; i <= 'z'; i++)
+for (c = 'A'; c <= 'Z'; c++)
+putchar(i);
+putchar(c);
+putchar('\n');
+return (0);
+}
+}
