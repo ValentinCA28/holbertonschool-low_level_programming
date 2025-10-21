401764a76631c0f5abb6b9a4c953f122e74dd5ba
ValentinCA28 <12286@holbertonstudents.com>
2025-10-21 18:41:14 +0200

    task 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+	* main - Entry point
+	*
+	* Description: print base16 hexadecimal
+	*
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+int c;
+
+for (c = 0 ; c < 10 ; c++)
+{
+	putchar(c + '0');
+}
+for (c = 'a' ; c <= 'f' ; c++)
+{
+	putchar(c);
+}
+return (0);
+}
