9831f5aa411c01281ecb42db97fcff9331ce8cfd
ValentinCA28 <12286@holbertonstudents.com>
2025-10-21 17:37:46 +0200

    task 7

---
+++ 7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+	* main - Entry point
+	*
+	* Description: print reverse de 0 a 9 - rev py
+	*
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+int c;
+
+for (c = 9; c >= 0 ; c--)
+{
+putchar(c + '0');
+}
+{
+putchar('\n');
+}
+return (0);
+}
+
+
