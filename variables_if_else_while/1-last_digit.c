f29311873724b9126da0524601f21494ea315245
ValentinCA28 <12286@holbertonstudents.com>
2025-10-21 13:09:34 +0200

    task 1 maj synthaxe

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 {
-printf("Last digit of", n, "is", lastDigit);
+printf("Last digit of %d is %d", n, lastDigit);
 }
 if (lastDigit > 5)
 {
@@ -25,7 +25,7 @@ printf("is greater than 5");
 }
 {
 if (lastDigit == 0)
-printf("and is 0")
+printf("and is 0");
 }
 {
 if (lastDigit < 5)
