4411fff7f2a51b984fb5486285d039863cead4fc
ValentinCA28 <12286@holbertonstudents.com>
2025-10-22 09:58:01 +0200

    task 9 final commit : pb de nom de fichier

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
 	* main - Entry point
 	*
-	* Description: print combinaison single digit
+* Description: print combinaison single digit
 	*
 	* Return: Always 0 (Success)
 	*/
 int main(void)
 {
-int c;
-for (c = 48 ; c <= 57; c++)
-{
-putchar(c);
-	if (c != 57)
-	putchar(',');
-	putchar(' ');
-}
-{
-putchar('\n');
-}
-return (0);
+	int c;
+	for (c = '0' ; c < '9' ; c++)
+	{
+			if (c != '9')		
+			{
+				putchar(',');
+			}
+	}
+	putchar(c);
+	putchar('\n');
+	return (0);
 }
