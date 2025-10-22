49d6d88643d3ac035f8673c68657627ae064552e
ValentinCA28 <12286@holbertonstudents.com>
2025-10-22 15:09:48 +0200

    task 100

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,30 @@
 #include <time.h>
 #include <stdio.h>
 /**
-	* main - Entry point
-	*
-	* Description: print alphabet minuscule et majuscules
-	*
-	* Return: Always 0 (Success)
-	*/
+ * main - Entry point
+ *
+ * Description: print alphabet minuscule et majuscules
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int c;
 int d;
 
 for (c = '0' ; c < '9' ; c++)
-for (d = '0' ; d < '9' ; d++)
 {
-putchar(c);
-putchar(d);
-if (c != '9')
-if (d != '9')
-{
-putchar(',');
-putchar(' ');
+ for (d = '1' ; d <= '9' ; d++)
+ {
+  putchar(c);
+  putchar(d);
+  if (!(c == '8' && d == '9'))
+  {
+   putchar(',');
+   putchar(' ');
+  }
+ }
 }
-}
+putchar('\n');
 return(0);
 }
-
