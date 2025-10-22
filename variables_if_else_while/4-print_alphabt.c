c2f38ef5e90e64d244ca59abc8446d1e09fb539a
ValentinCA28 <12286@holbertonstudents.com>
2025-10-22 10:36:05 +0200

    task4 commit

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+	* main - Entry point
+	*
+	* Description: print reverse de 0 a 9 - rev py
+	*
+	* Return: Always 0 (Success)
+	*/
+int main(void)
+{
+int c;
+
+for (c = 'a' ; c <= 'z' ; c++)
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+{
+putchar('\n');
+}
+return (0);
+}
