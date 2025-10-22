76e0b4405bb7265925572932ad123fd727eaac8b
ValentinCA28 <12286@holbertonstudents.com>
2025-10-22 16:04:16 +0200

    fix: correct formatting in main function and improve readability

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
-	* main - Entry point
-	*
-	* Description: print combinaison 3 chiffres
-	*
-	* Return: Always 0 (Success)
-	*/
+* main - Entry point
+*
+* Description: print combinaison 3 chiffres
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int c;
@@ -16,16 +16,16 @@ int e;
 for (c = 0; c <= 8; c++)
 for (d = c + 1; d <= 9; d++)
 for (e = d + 1; e <= 9; e++)
-	{
-		putchar(c + '0');
-		putchar(d + '0');
-		putchar(e + '0');
-	if (!(c == 7 && d == 8 && e == 9))
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
+{
+putchar(c + '0');
+putchar(d + '0');
+putchar(e + '0');
+if (!(c == 7 && d == 8 && e == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
 putchar('\n');
 return (0);
 }
