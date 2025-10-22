f89728f3b22f7e70d2d63aafaa86953044cfe8cb
ValentinCA28 <12286@holbertonstudents.com>
2025-10-22 15:14:14 +0200

    betty style

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Description: print alphabet minuscule et majuscules
- *
- * Return: Always 0 (Success)
- */
+	* main - Entry point
+	*
+	* Description: print alphabet minuscule et majuscules
+	*
+	* Return: Always 0 (Success)
+	*/
 int main(void)
 {
 int c;
@@ -15,17 +15,19 @@ int d;
 
 for (c = '0' ; c < '9' ; c++)
 {
- for (d = '1' ; d <= '9' ; d++)
- {
-  putchar(c);
-  putchar(d);
-  if (!(c == '8' && d == '9'))
-  {
-   putchar(',');
-   putchar(' ');
-  }
- }
+	for (d = '0' ; d <= '9' ; d++)
+	{
+		if ((c == '0') && (d == '0'))
+			continue;
+		putchar(c);
+		putchar(d);
+		if (!(c == '8' && d == '9'))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
 }
 putchar('\n');
-return(0);
+return (0);
 }
