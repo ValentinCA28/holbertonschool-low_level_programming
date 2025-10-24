75603e1ea683ab39187dbfc253accae4e787e004
ValentinCA28 <12286@holbertonstudents.com>
2025-10-24 12:58:58 +0200

    task 3 4 5

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
-* _isalpha - Vérifie si le caractère passé est une lettre de l'alphabet
-* @c: Le caractère à vérifier
-*
-* Return: 1 si c est une lettre (majuscule ou minuscule), 0 sinon.
-*/
+ * _isalpha - Vérifie si le caractère passé est une lettre de l'alphabet
+ * @c: Le caractère à vérifier
+ *
+ * Return: 1 si c est une lettre (majuscule ou minuscule), 0 sinon.
+ */
 int _isalpha(int c)
 
 {
-        if (c >= 'a' && c <= 'z')
-        {
-                return (1);
-        }
-        else if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-        {
-                return (0);
-        }
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
