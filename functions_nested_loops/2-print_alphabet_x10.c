26253fcd069d3735aa4fd8c223531d806c87aa23
ValentinCA28 <12286@holbertonstudents.com>
2025-10-24 09:42:01 +0200

    task 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,16 @@
 void print_alphabet_x10(void)
 
 {
-	int x;
-	char c;
-	for (x = 0; x < 10; x++)
-	{
-		for (c = 'a'; c <= 'z'; c++)
-			{
-				_putchar(c);
-			}	
-			_putchar('\n');
-		}
+int x;
+char c;
+
+for (x = 0; x < 10; x++)
+
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
 }
