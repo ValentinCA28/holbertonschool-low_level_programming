93f8830d686be42e39972d05c72ce5f2f1a75a27
ValentinCA28 <12286@holbertonstudents.com>
2025-10-24 14:05:21 +0200

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-* print_last_digit - Affiche le dernier chiffre d'un nombre
-* @a: Le nombre dont on veut afficher le dernier chiffre
-*
-* Cette fonction calcule le dernier chiffre du nombre passé en paramètre,
-* l'affiche à l'aide de _putchar et retourne ce chiffre (toujours positif).
-*
-* Retour: Le dernier chiffre du nombre (toujours positif)
-*/
+ * print_last_digit - Affiche le dernier chiffre d'un nombre
+ * @a: Le nombre dont on veut afficher le dernier chiffre
+ *
+ * Cette fonction calcule le dernier chiffre du nombre passé en paramètre,
+ * l'affiche à l'aide de _putchar et retourne ce chiffre (toujours positif).
+ *
+ * Retour: Le dernier chiffre du nombre (toujours positif)
+ */
 int print_last_digit(int a)
 
 {
