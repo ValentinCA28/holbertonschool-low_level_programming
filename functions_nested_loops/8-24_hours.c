e645671980ae5d155b3a44a287d055446fdb1399
ValentinCA28 <12286@holbertonstudents.com>
2025-10-26 09:25:52 +0100

    task 8 printf > write

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,16 +1,23 @@
 #include "main.h"
-#include <stdio.h>
+#include <unistd.h>
 
 void jack_bauer(void)
 
 {
+	char buffer [6];
 	int a, b;
 
 	for (a = 0; a < 24; a++)
 	{
-
-		for (b = 0; b < 60; b++)
-
-		printf("%02d:%02d\n", a, b);
+		for(b = 0; b < 60; b++)
+		{
+			buffer[0] = ('0' + (a / 10));
+			buffer[1] = ('0' + (a % 10));
+			buffer[2] = (':');
+			buffer[3] = ('0' + (b / 10));
+			buffer[4] = ('0' + (b % 10));
+			buffer[5] = ('\n');
+			write(1, buffer, 6);
+		}
 	}
 }
