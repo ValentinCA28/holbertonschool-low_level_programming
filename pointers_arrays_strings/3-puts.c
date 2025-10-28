7dc317a17a9f5166c6d3279a11cdd4ce4afd3bf0
ValentinCA28 <12286@holbertonstudents.com>
2025-10-28 15:10:32 +0100

    task 3

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - prints a string followed by a new line
+ * @str: pointer to the string to be printed
+ *
+ * Description: Prints each character of the string using _putchar
+ * until it reaches the null terminator, then prints a newline
+ * Return: void
+ */
 void _puts(char *str)
 {
-	char x;
-	x = *str;
-	printf("%c", x);
+	while(*str != '\0')
+		{
+			_putchar(*str);
+			str++;
+		}
+		_putchar('\n');
 }
