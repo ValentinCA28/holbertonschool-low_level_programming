eabccb65ac6e7d1e9e6a5940030b0542cb94aeeb
ValentinCA28 <12286@holbertonstudents.com>
2025-10-29 14:15:45 +0100

    task 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <limits.h>
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int x = 0;
+
+	if (s[i] >= 0)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+			if(s[i] == ' ')
+			{
+				i += 1;
+			}
+			if(s[i] = "-")
+			{
+				s[i] = s[-i]*
+				i++;
+			}
+			else
+			{
+				i++;
+			}
+
+	}
+}
