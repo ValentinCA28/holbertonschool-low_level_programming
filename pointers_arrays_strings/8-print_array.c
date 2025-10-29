147d16fcbd79b5538d0dd89c1e83bf87aa47613f
ValentinCA28 <12286@holbertonstudents.com>
2025-10-29 11:19:15 +0100

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	int len;
+
+	while(a[len] != '\0')
+	{
+		a++;
+	}
+	for(n = 0; i < n; n++)
+	{
+
+		printf("%d", n[i]);
+
+	}
+}
