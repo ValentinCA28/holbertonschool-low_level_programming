2181b936c3715c9fac3c1982071484f6530c1fd7
ValentinCA28 <12286@holbertonstudents.com>
2025-10-29 21:15:50 +0100

    task 100

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,33 +2,32 @@
 #include <limits.h>
 #include <stdio.h>
 
-
 int _atoi(char *s)
 {
 	int i = 0;
 	int signe = 1;
 	unsigned int temp = 0;
+	int found_digit = 0;
 
-
-			while (s[i] == ' ')
+	while (s[i])
+	{
+		if (s[i] == '-')
+			signe = -signe;
+		else if (s[i] == '+')
+			;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			if (temp > (unsigned int)INT_MAX / 10 || 
+			    (temp == (unsigned int)INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10)))
 			{
-				i++;
+				return (signe == 1 ? INT_MAX : INT_MIN);
 			}
-			while (s[i] == '-' || s[i] == '+')
-			{
-				if(s[i] == '-')
-				signe = -signe;
-				i++;
-			}
-			while (s[i] >= '0' && s[i] <= '9')
-				{
-				if (temp > (unsigned int)INT_MAX / 10 || (temp == (unsigned int)INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10)))
-					{
-    				return (signe == 1 ? INT_MAX : INT_MIN);
-					}
-					temp = temp * 10 + (s[i] - '0');
-					i++;
-				}
-
-		return signe * (int)temp;
+			temp = temp * 10 + (s[i] - '0');
+		}
+		else if (found_digit)
+			break;
+		i++;
+	}
+	return signe * (int)temp;
 }
---
+++ pointers_arrays_strings/test.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <limits.h>
+#include <stdio.h>
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int signe = 1;
+	unsigned int temp = 0;
+
+	while (s[i] == ' ')
+	{
+		i++;
+	}
+	while (s[i] == '-' || s[i] == '+')
+	{
+		if (s[i] == '-')
+			signe = -signe;
+		i++;
+	}
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (temp > (unsigned int)INT_MAX / 10 || (temp == (unsigned int)INT_MAX / 10 && (s[i] - '0') > (INT_MAX % 10)))
+		{
+			return (signe == 1 ? INT_MAX : INT_MIN);
+		}
+		temp = temp * 10 + (s[i] - '0');
+		i++;
+	}
+	return signe * (int)temp;
+}
