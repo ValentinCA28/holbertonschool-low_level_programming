6635f2772a74397ba6dd474774c434b602c1734c
ValentinCA28 <12286@holbertonstudents.com>
2025-11-02 10:29:03 +0100

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: String to modify
+ * Return: Pointer to modified string
+ */
+char *cap_string(char *s)
+
+{
+	char *start = s;
+	int temp = 0;
+
+	for (; *s; s++)
+	{
+		if (*s == ' ' || *s == '\t' || *s == '\n' ||
+			*s == ',' || *s == ';' || *s == '.' ||
+			*s == '!' || *s == '?' || *s == '"' ||
+			*s == '(' || *s == ')' || *s == '{' || *s == '}')
+		{
+			temp = 1;
+		}
+
+		else if (temp && *s >= 'a' && *s <= 'z')
+		{
+			*s -= 32;
+			temp = 0;
+		}
+
+		else
+		{
+			temp = 0;
+		}
+	}
+	return (start);
+}
