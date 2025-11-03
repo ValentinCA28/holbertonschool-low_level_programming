704066214766d39a6d1f12a201b61454f4c39171
ValentinCA28 <12286@holbertonstudents.com>
2025-11-03 13:27:59 +0100

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates the first occurrence of a character in a string
+ * @s: Pointer to the string to search
+ * @c: Character to locate
+ *
+ * Return: Pointer to the first occurrence of c in s,
+ *         or NULL if the character is not found.
+ *         If c is '\0', returns a pointer to the terminator.
+ */
+char *_strchr(char *s, char c)
+
+{
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+
+	if (c == '\0')
+	{
+		return (s);
+	}
+	}
+	return (NULL);
+}
