1ecd19cd39863c2760bdaf47cdcd1f20df45a959
ValentinCA28 <12286@holbertonstudents.com>
2025-11-03 15:12:29 +0100

    task 4

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -10,19 +10,18 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    int i;
+	int i;
 
-    while (*s != '\0')
-    {
-        for (i = 0; accept[i] != '\0'; i++)
-        {
-            if (*s == accept[i])
-            {
-                return (s);
-            }
-        }
-        s++;
-    }
-
-    return (NULL);
+	while (*s != '\0')
+	{
+	for (i = 0; accept[i] != '\0'; i++)
+	{
+		if (*s == accept[i])
+		{
+			return (s);
+		}
+	}
+	s++;
+	}
+	return (NULL);
 }
