a16887e65a255532d6ec6fb81e43212d44f4bfa7
ValentinCA28 <12286@holbertonstudents.com>
2025-11-05 18:44:12 +0100

    task 6

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+	return (-1);
+	}
+	if (n == 0)
+	{
+		return (n);
+	}
+	if (n == 1)
+	{
+		return (n);
+	}
+		else
+		{
+			return (-1);
+		}
+	}
+
+}
