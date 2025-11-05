6ddf922fa99a32f1fc0e74a528bad57d82ddbd5e
ValentinCA28 <12286@holbertonstudents.com>
2025-11-05 20:40:54 +0100

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,23 +1,34 @@
 #include "main.h"
 
+/**
+ * find_sqrt - recursively finds if x^2 == n
+ * @n: number to find square root of
+ * @x: current guess (starts at 1)
+ *
+ * Return: the natural square root, or -1 if none
+ */
+int find_sqrt(int n, int x)
+{
+	if (x * x == n)
+		return (x);
+	if (x * x > n)
+		return (-1);
+	return (find_sqrt(n, x + 1));
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to calculate the square root of
+ *
+ * Return: the resulting square root, or -1 if no natural root
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
-	return (-1);
-	}
+		return (-1);
 	if (n == 0)
-	{
-		return (n);
-	}
+		return (0);
 	if (n == 1)
-	{
-		return (n);
-	}
-		else
-		{
-			return (-1);
-		}
-	}
-
+		return (1);
+	return (find_sqrt(n, 1));
 }
