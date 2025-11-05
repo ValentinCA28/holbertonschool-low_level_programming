d278ed19e6a0986fa3c03822f24c47f507cedd73
ValentinCA28 <12286@holbertonstudents.com>
2025-11-05 20:22:14 +0100

    task 6

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,3 +1,25 @@
+#include "main.h"
+
+/**
+ * check_prime - recursively checks if n is prime starting from div
+ * @n: number to check
+ * @div: current divisor to test
+ *
+ * Return: 1 if n is prime, 0 otherwise
+ */
+int check_prime(int n, int div)
+{
+	if (div * div > n)
+	{
+		return (1);
+	}
+	if (n % div == 0)
+	{
+		return (0);
+	}
+	return check_prime(n, div + 2);
+}
+
 /**
  * is_prime_number - checks if a number is prime
  * @n: the number to check
@@ -6,26 +28,17 @@
  */
 int is_prime_number(int n)
 {
-int i;
-
-if (n <= 1)
-{
-	return (0);
-}
-if (n == 2)
-{
-	return (1);
-}
-if (n % 2 == 0)
-{
-	return (0);
-}
-for (i = 3; i * i <= n; i += 2)
-{
-	if (n % i == 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-}
-return (1);
+	if (n == 2)
+	{
+		return (1);
+	}
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	return check_prime(n, 3);
 }
