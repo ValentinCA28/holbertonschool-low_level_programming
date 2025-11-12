ebb4ae370671abb806af3750e78bf7cd11cdad31
ValentinCA28 <12286@holbertonstudents.com>
2025-11-12 21:58:07 +0100

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
