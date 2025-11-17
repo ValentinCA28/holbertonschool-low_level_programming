126727195b5b5bdfc0769fe2bee6cabc7a915568
ValentinCA28 <12286@holbertonstudents.com>
2025-11-17 15:24:06 +0100

    task 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects the correct function to perform the operation
+ * @s: the operator passed as argument (as a string)
+ *
+ * Return: a pointer to the corresponding function
+ *         NULL if the operator is not found
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+	int i = 0;
+	if (s == NULL || s[1] != '\0')
+	{
+		return (NULL);
+	}
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op[0] == s[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	return (NULL);
+}
---
+++ function_pointers/3-main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calc.h"
+
+/**
+ * main - simple calculator
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, or error code
+ */
+int main(int argc, char *argv[])
+{
+    int a, b, result;
+    int (*operation)(int, int);
+
+    /* Vérification du nombre exact d'arguments */
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    /* Récupération et conversion des opérandes */
+    a = atoi(argv[1]);
+    b = atoi(argv[3]);
+
+    /* Récupération du pointeur vers la bonne fonction */
+    operation = get_op_func(argv[2]);
+
+    /* Vérification que l'opérateur est valide */
+    if (operation == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    /* Vérification de la division/modulo par zéro */
+    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    /* Exécution de l'opération et affichage du résultat */
+    result = operation(a, b);
+    printf("%d\n", result);
+
+    return (0);
+}
