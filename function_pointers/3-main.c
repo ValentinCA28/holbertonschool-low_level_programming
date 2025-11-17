53913f7a5c611458370a15e8bff52e7f5d147171
ValentinCA28 <12286@holbertonstudents.com>
2025-11-17 15:34:06 +0100

    task 3 betty

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,40 +11,40 @@
  */
 int main(int argc, char *argv[])
 {
-    int a, b, result;
-    int (*operation)(int, int);
+	int a, b, result;
+	int (*operation)(int, int);
 
-    /* Vérification du nombre exact d'arguments */
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	/* Vérification du nombre exact d'arguments */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    /* Récupération et conversion des opérandes */
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
+	/* Récupération et conversion des opérandes */
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
 
-    /* Récupération du pointeur vers la bonne fonction */
-    operation = get_op_func(argv[2]);
+	/* Récupération du pointeur vers la bonne fonction */
+	operation = get_op_func(argv[2]);
 
-    /* Vérification que l'opérateur est valide */
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	/* Vérification que l'opérateur est valide */
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    /* Vérification de la division/modulo par zéro */
-    if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	/* Vérification de la division/modulo par zéro */
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    /* Exécution de l'opération et affichage du résultat */
-    result = operation(a, b);
-    printf("%d\n", result);
+	/* Exécution de l'opération et affichage du résultat */
+	result = operation(a, b);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
