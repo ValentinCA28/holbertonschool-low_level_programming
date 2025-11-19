2a77049cf5ef3ddf236e9469d45bd12148aa9ebf
ValentinCA28 <12286@holbertonstudents.com>
2025-11-19 17:31:40 +0100

    task 2 "nil" > "(nil)"

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		const char *x = va_arg(args, const char *);
 		if (x == NULL)
 		{
-			printf("nil");
+			printf("(nil)");
 		}
 		else
 		{
