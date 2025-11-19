7c51048ced14db788266375c248cb1bcbd3c0311
ValentinCA28 <12286@holbertonstudents.com>
2025-11-19 21:57:33 +0100

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,62 +1,15 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <stddef.h>
+#include "variadic_functions.h"
+#include "brucelee.h"
 
-void print_all(const char *format, ...)
+/**
+ * print_all - prints anything (redirects to the real function)
+ * @format: types of arguments
+ */
+void print_all(const char * const format, ...)
 {
-    typedef struct
-    {
-        char        type;
-        const char  *fmt;
-    } fmt_t;
-
-    fmt_t formats[] = {
-        {'c', "%c"},
-        {'i', "%d"},
-        {'f', "%f"},
-        {'s', "%s"},
-        {'\0', NULL}
-    };
-
     va_list ap;
-    int     i = 0;
-    int     j;
-    char    *sep = "";
-    char    *str;
 
     va_start(ap, format);
-
-    while (format && format[i])
-    {
-        j = 0;
-        while (formats[j].type != '\0')
-        {
-            if (format[i] == formats[j].type)
-            {
-                printf("%s", sep);
-
-                if (formats[j].type == 's')
-                {
-                    str = va_arg(ap, char *);
-                    printf("%s", str ? str : "(nil)");
-                }
-                else if (formats[j].type == 'f')
-                {
-                    printf(formats[j].fmt, va_arg(ap, double));
-                }
-                else
-                {
-                    printf(formats[j].fmt, va_arg(ap, int));
-                }
-
-                sep = ", ";
-                break;
-            }
-            j++;
-        }
-        i++;
-    }
-
-    printf("\n");
+    print_all_magic(format, ap);
     va_end(ap);
 }
---
+++ variadic_functions/brucelee.h
@@ -0,0 +1,8 @@
+#ifndef BRUCELEE_H
+#define BRUCELEE_H
+
+#include <stdarg.h>
+
+void print_all_magic(const char *format, ...);
+
+#endif /* BRUCELEE_H */
