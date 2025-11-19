a00bbf7ca7d72fadb3cf498f0114e9067dd48cca
ValentinCA28 <12286@holbertonstudents.com>
2025-11-19 17:01:29 +0100

    task 1 push betty

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -13,19 +13,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 	int x;
+
 	va_start(args, n);
-
-
 
 	for (i = 0; i < n; i++)
 	{
 		x = va_arg(args, int);
 		printf("%d", x);
-		if(separator != NULL && i < n - 1)
+		if (separator != NULL && i < n - 1)
 		{
 			printf("%s", separator);
 		}
-
 	}
 	printf("\n");
 	va_end(args);
