6814fa97e962ab86de2cdd1165e4afb24e7fbfd9
ValentinCA28 <12286@holbertonstudents.com>
2025-11-20 11:21:26 +0100

    task 4 test array type argc argv

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,15 +1,57 @@
 #include "variadic_functions.h"
-#include "brucelee.h"
+#include <stdarg.h>
+#include <stdio.h>
 
-/**
- * print_all - prints anything (redirects to the real function)
- * @format: types of arguments
- */
-void print_all(const char * const format, ...)
+typedef struct {
+    char spec;
+    char *fmt;
+} format_t;
+
+void print_all(const char *format, ...)
 {
-    va_list ap;
+    format_t check[] = {
+        {'c', "%c"},
+        {'i', "%d"},
+        {'f', "%f"},
+        {'s', "%s"},
+        {'\0', NULL}
+    };
 
-    va_start(ap, format);
-    print_all_magic(format, ap);
-    va_end(ap);
+    va_list args;
+    int i = 0;
+    char *sep = "";
+    char *s;
+
+    va_start(args, format);
+
+    while (format && format[i])
+    {
+        int j = 0;
+        while (check[j].spec != '\0')
+        {
+            if (format[i] == check[j].spec)
+            {
+                printf("%s", sep);
+                sep = ", ";
+
+                if (check[j].spec == 's')
+                {
+                    s = va_arg(args, char *);
+                    printf("%s", s ? s : "(nil)");
+                }
+                if (check[j].spec != 's')
+                {
+                    if (check[j].spec == 'f')
+                        printf(check[j].fmt, va_arg(args, double));
+                    else
+                        printf(check[j].fmt, va_arg(args, int));
+                }
+                break;
+            }
+            j++;
+        }
+        i++;
+    }
+    printf("\n");
+    va_end(args);
 }
--- variadic_functions/brucelee.h
+++
@@ -1,8 +0,0 @@
-#ifndef BRUCELEE_H
-#define BRUCELEE_H
-
-#include <stdarg.h>
-
-void print_all_magic(const char *format, ...);
-
-#endif /* BRUCELEE_H */
