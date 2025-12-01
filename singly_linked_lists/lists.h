c1be60e0e17c9ef1de83c7b942c3564bd4462ac8
ValentinCA28 <12286@holbertonstudents.com>
2025-12-01 20:48:36 +0100

    task 1

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LIST_H
 
 #include <stddef.h>
+#include <stdio.h>
 
 /**
  * struct list_s - singly linked list
@@ -19,5 +20,6 @@ typedef struct list_s
 } list_t;
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 
 #endif  /* LIST_H*/
