c31a2d5759eba7b3f987fe786ff276e7e8d156d3
ValentinCA28 <12286@holbertonstudents.com>
2025-12-01 15:23:36 +0100

    task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,19 +6,18 @@ size_t print_list(const list_t *h)
 
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		if (h == NULL)
+		if (h->str == NULL)
 		{
-			printf("[0], (nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
-			printf("%u, %s\n", h->len, h->str);
-			h = h -> next;
-			count++;
-
+			printf("[%u] %s\n", h->len, h->str);
 		}
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
