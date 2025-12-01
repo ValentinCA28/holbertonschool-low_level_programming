4966490b8aca700e6d9aa413b94b359e3fd3c6f7
ValentinCA28 <12286@holbertonstudents.com>
2025-12-01 21:55:04 +0100

    task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -6,16 +6,22 @@
 list_t *add_node(list_t **head, const char *str)
 {
 
-	list_t *new = malloc(sizeof(list_t));
+	list_t *new;
 	unsigned int len = 0;
 
-	if (head == NULL || str == NULL)
+	if (!head)
 	{
 		return (NULL);
 	}
 
-	new->str = strdup(str);
-	if (!new -> str)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->str = strdup(str); /* copie de str*/
+	if (new->str == NULL)
 	{
 		free(new);
 		return (NULL);
@@ -26,8 +32,8 @@ list_t *add_node(list_t **head, const char *str)
             len++;
     }
 	new->len = len;
-	new->next = *head;
 
+	new->next = *head;
 	*head = new;
 
 	return (new);
