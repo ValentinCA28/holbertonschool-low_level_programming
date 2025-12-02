f280d2049647eef5e903afa22fbf67cea3cbeac4
ValentinCA28 <12286@holbertonstudents.com>
2025-12-02 08:57:58 +0100

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next;
+
+	current = head;
+
+	while(current !=NULL)
+	{
+		next = current->next;
+		free(current->next);
+		free(current);
+		current = next;
+	}
+
+
+	}
