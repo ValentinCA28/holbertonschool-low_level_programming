106c6a8acb25686cdb8f07eae40c59102c2397b4
ValentinCA28 <12286@holbertonstudents.com>
2025-12-02 11:21:35 +0100

    task 0

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,11 +1,5 @@
 #include "lists.h"
 
-/**
- * free_list - frees a list_t list
- * @head: pointer to the head of the list
- *
- * Return: void
- */
 void free_list(list_t *head)
 {
 	list_t *current;
