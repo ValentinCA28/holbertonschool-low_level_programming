332bc8d8aea1c3e2d9ecad345a71c8d1a7b54bc4
ValentinCA28 <12286@holbertonstudents.com>
2025-12-03 17:15:56 +0100

    task 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -42,9 +42,5 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		current = current->next;
 		i++;
 	}
-	if (idx == 0)
-	{
-		return (add_dnodeint(h, n));
-	}
 	return (NULL);
 }
